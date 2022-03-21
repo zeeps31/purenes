5a55c681efaf18e63bd63634e35b2657b5d1f055
Stephen Brady <stephen.brady86@gmail.com>
2022-03-21 09:22:09 -0500

    Add semantic versioning

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
+import semver
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='nespy',
-      version='0.1.0',
+version = semver.VersionInfo.parse('0.2.0')
+
+setup(name='purenes',
+      version=str(version),
       description='A NES emulator in Python',
       author='Stephen Brady',
       author_email='stephen.brady86@gmail.com',
+      setup_requires=["semver>=2.13.0"],
       packages=find_packages(exclude=["tests"]),
       )
