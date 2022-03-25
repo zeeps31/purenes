e6c022a14e787f418ef02c3c86a264b034eebea2
Stephen Brady <stephen.brady86@gmail.com>
2022-03-25 11:06:16 -0500

    Add typing_extensions 4.1.1 as dependency

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import semver
 from setuptools import setup
 from setuptools import find_packages
 
-version = semver.VersionInfo.parse('0.4.0')
+version = semver.VersionInfo.parse('0.4.1')
 
 setup(name='purenes',
       version=str(version),
       description='A NES emulator in Python',
       author='Stephen Brady',
       author_email='stephen.brady86@gmail.com',
-      install_requires=["numpy==1.21.5"],
+      install_requires=["numpy==1.21.5", "typing_extensions==4.1.1"],
       test_requires=["pytest>=7.1.1"],
       setup_requires=["semver>=2.13.0"],
       packages=find_packages(exclude=["tests"]),
