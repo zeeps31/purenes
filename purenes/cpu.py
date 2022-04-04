f75377ec776b2fc80e9adb2758f98971b461087b
Stephen Brady <stephen.brady86@gmail.com>
2022-04-04 11:19:59 -0500

    test

--- purenes/cpu.py
+++ purenes/cpu.py
@@ -15,19 +15,24 @@ class CPUBus(object):
 
     https://www.nesdev.org/wiki/CPU_memory_map.
 
-    +-----------------------------------------------------------------------+
-    | Address range  | Size | Device                                        |
-    +================+======+===============================================+
-    | $0000-$07FF	 |$0800 | 2KB internal RAM                              |
-    | $0800-$0FFF	 |$0800 | Mirrors of $0000-$07FF                        |
-    | $1000-$17FF	 |$0800 |                                               |
-    | $1800-$1FFF	 |$0800 |                                               |
-    | $2000-$2007	 |$0008 | NES PPU registers                             |
-    | $2008-$3FFF	 |$1FF8 | Mirrors of $2000-2007 (repeats every 8 bytes) |
-    | $4000-$4017	 |$0018 | NES APU and I/O registers                     |
-    | $4018-$401F	 |$0008 | APU and I/O functionality.                    |
-    | $4020-$FFFF	 |$BFE0 | Cartridge space: PRG ROM, PRG RAM, and mapper |
-    +----------------+------+-----------------------------------------------+
+    Address range Size	Device
+    $0000-$07FF	  $0800	2KB internal RAM
+    $0800-$0FFF	  $0800	Mirrors of $0000-$07FF
+    $1000-$17FF	  $0800
+    $1800-$1FFF	  $0800
+    $2000-$2007	  $0008	NES PPU registers
+    $2008-$3FFF	  $1FF8	Mirrors of $2000-2007 (repeats every 8 bytes)
+    $4000-$4017	  $0018	NES APU and I/O registers
+    $4018-$401F	  $0008	APU and I/O functionality.
+    $4020-$FFFF	  $BFE0	Cartridge space: PRG ROM, PRG RAM, and mapper
+
+    Methods
+    -------
+    read(address: int) -> int:
+        Reads an address from a device connected to the CPU bus.
+
+    write(address: int, data: int) -> None:
+        Writes to an address and device connected to the CPU bus.
     """
     # TODO: https://github.com/zeeps31/purenes/issues/6
     _INVALID_ADDRESS_EXCEPTION: Final = ("Invalid address provided: "
