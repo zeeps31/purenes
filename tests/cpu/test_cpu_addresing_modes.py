16c98191ea238dc83d09078d3ffdd73c11ad5009
Stephen Brady <stephen.brady86@gmail.com>
2022-06-13 18:53:24 -0500

    feat(cpu): implement accumulator addressing mode (#90)
    Implements accumulator addressing mode and updates the ASL operation to account for scenarios where accumulator addressing is being used. Adds test coverage.

--- tests/cpu/test_cpu_addresing_modes.py
+++ tests/cpu/test_cpu_addresing_modes.py
@@ -6,6 +6,29 @@ import pytest_mock
 import purenes.cpu
 
 
+def test_accumulator_addressing_mode(
+        cpu: purenes.cpu.CPU,
+        mock_cpu_bus: mock.Mock,
+        mocker: pytest_mock.MockFixture):
+    """Tests accumulator addressing mode using opcode 0x0A.
+
+    Verifies the following:
+
+    1. The accumulator is set as the operation value.
+    """
+    # Patch out the execution of the operation
+    mocker.patch.object(cpu, "_execute_operation")
+
+    cpu.pc = 0x0000
+    cpu.a = 0xFF
+
+    mock_cpu_bus.read.return_value = 0x0A  # Opcode
+
+    cpu.clock()
+
+    assert cpu.operation_value == cpu.a
+
+
 @pytest.mark.parametrize(
     "operand",
     [
